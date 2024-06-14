809e86ceec68ba2b6b2cd7c5c06853f75b0cb323
nsirtak <nsirtak@gmail.com>
2024-06-13 19:24:27 -0500

    scrabble scorer

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,4 +1,3 @@
-// This assignment is inspired by a problem on Exercism (https://exercism.org/tracks/javascript/exercises/etl) that demonstrates Extract-Transform-Load using Scrabble's scoring system. 
 
 const input = require("readline-sync");
 
@@ -36,24 +35,111 @@ function initialPrompt() {
    console.log("Let's play some scrabble! Enter a word:");
 };
 
-let newPointStructure;
+// let newPointStructure;
 
-let simpleScorer;
+// let simpleScorer;
 
-let vowelBonusScorer;
+// let vowelBonusScorer;
 
-let scrabbleScorer;
+// let scrabbleScorer;
 
-const scoringAlgorithms = [];
+// const scoringAlgorithms = [];
 
-function scorerPrompt() {}
+// function scorerPrompt() {}
 
-function transform() {};
+// function transform() {};
 
-function runProgram() {
-   initialPrompt();
+// function runProgram() {
+//    initialPrompt();
    
-}
+// }
+let newPointStructure = {
+   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+   2: ['D', 'G'],
+   3: ['B', 'C', 'M', 'P'],
+   4: ['F', 'H', 'V', 'W', 'Y'],
+   5: ['K'],
+   8: ['J', 'X'],
+   10: ['Q', 'Z']
+ };
+ 
+ function simpleScorer(word) {
+   word = word.toUpperCase();
+   let score = 0;
+ 
+   for (let i = 0; i < word.length; i++) {
+     for (const pointValue in newPointStructure) {
+       if (newPointStructure[pointValue].includes(word[i])) {
+         score += parseInt(pointValue);
+       }
+     }
+   }
+   return score;
+ }
+ 
+ function vowelBonusScorer(word) {
+   word = word.toUpperCase();
+   let score = 0;
+ 
+   for (let i = 0; i < word.length; i++) {
+     if (word[i] === 'A' || word[i] === 'E' || word[i] === 'I' || word[i] === 'O' || word[i] === 'U') {
+       score += 2;
+     } else {
+       for (const pointValue in newPointStructure) {
+         if (newPointStructure[pointValue].includes(word[i])) {
+           score += parseInt(pointValue);
+         }
+       }
+     }
+   }
+   return score;
+ }
+ 
+ function scrabbleScorer(word) {
+   word = word.toUpperCase();
+   let score = 0;
+ 
+   for (let i = 0; i < word.length; i++) {
+     if (word[i] === 'A' || word[i] === 'E' || word[i] === 'I' || word[i] === 'O' || word[i] === 'U') {
+       score += 2;
+     } else {
+       for (const pointValue in newPointStructure) {
+         if (newPointStructure[pointValue].includes(word[i])) {
+           score += parseInt(pointValue);
+         }
+       }
+     }
+   }
+   return score;
+ }
+ 
+ const scoringAlgorithms = [oldScrabbleScorer, simpleScorer, vowelBonusScorer, scrabbleScorer];
+ 
+ function scorerPrompt() {
+   console.log("Choose a scoring algorithm:");
+   console.log("1. Old Scrabble Scorer");
+   console.log("2. Simple Scorer");
+   console.log("3. Vowel Bonus Scorer");
+   console.log("4. Scrabble Scorer");
+ }
+ 
+ function transform(word) {
+   let algorithmChoice = input.question("Enter the number of your chosen algorithm:");
+   let chosenAlgorithm = scoringAlgorithms[algorithmChoice - 1];
+   console.log(chosenAlgorithm(word));
+ }
+ function initialPrompt() {
+   console.log("Let's play some scrabble! Enter a word:");
+ }
+ 
+ function runProgram() {
+   initialPrompt();
+   let word = input.question("Enter a word:");
+   scorerPrompt();
+   transform(word);
+ }
+ 
+ runProgram();
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
